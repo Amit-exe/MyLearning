e568f220824f4e4d239c61aea148e2baa0ed3c82
Amit-exe <amit.kushwaha.exe@gmail.com>
2025-02-08 01:40:25 +0530

    more dry code in workout route

--- Gym-Buddy/backend/routes/workout.js
+++ Gym-Buddy/backend/routes/workout.js
@@ -1,21 +1,35 @@
+// const express = require("express");
+// const {
+//   createWorkout,
+//   getSingleWorkout,
+//   getAllWorkout,
+//   deleteWorkout,
+//   updateWorkout,
+// } = require("../controller/workoutController");
+// const router = express.Router();
+
+// router.get("/", getAllWorkout);
+
+// router.get("/:id", getSingleWorkout);
+
+// router.post("/", createWorkout);
+
+// router.patch("/:id", updateWorkout);
+
+// router.delete("/:id", deleteWorkout);
+
+// module.exports = router;
+
 const express = require("express");
-const {
-  createWorkout,
-  getSingleWorkout,
-  getAllWorkout,
-  deleteWorkout,
-  updateWorkout,
-} = require("../controller/workoutController");
+const ctrlw = require("../controller/workoutController");
 const router = express.Router();
 
-router.get("/", getAllWorkout);
+router.route("/").get(ctrlw.getAllWorkout).post(ctrlw.createWorkout);
 
-router.get("/:id", getSingleWorkout);
-
-router.post("/", createWorkout);
-
-router.patch("/:id", updateWorkout);
-
-router.delete("/:id", deleteWorkout);
+router
+  .route("/:id")
+  .get(ctrlw.getSingleWorkout)
+  .patch(ctrlw.updateWorkout)
+  .delete(ctrlw.deleteWorkout);
 
 module.exports = router;
